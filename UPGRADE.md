98f93da89cfeeb797d08892a61a94db6245599dc
batyrmastyr <batyrmastyr@gmail.com>
2025-04-09 12:53:31 +0400

    fix tests, revert changes to rector, update CHANGELOG and UPGRADE

--- UPGRADE.md
+++ UPGRADE.md
@@ -7,6 +7,10 @@ application when you upgrade the package from one version to another.
 > to upgrade from version A to version C and there is version B between A and C, you need
 > to following the instructions for both A and B.
 
+## Upgrade from 3.x
+
+- If you defined custom `yiisoft/error-handler`'s `ErrorCatcher` DI configuration replace it with `ThrowableResponseFactory`.
+
 ## Upgrade from 2.x
 
 - `RoadRunnerApplicationRunner` has been renamed to `RoadRunnerHttpApplicationRunner`
--- rector.php
+++ rector.php
@@ -5,20 +5,28 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
 
-return RectorConfig::configure()
-    ->withPaths([
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ])
-    ->withPhpSets(php81: true)
-    ->withRules([
-        InlineConstructorDefaultToPropertyRector::class,
-    ])
-    ->withSkip([
-        ClosureToArrowFunctionRector::class,
-        ReadOnlyPropertyRector::class,
-        NullToStrictStringFuncCallArgRector::class,
     ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/tests/Support/Grpc/GPBMetadata',
+        __DIR__ . '/tests/Support/Grpc/Service',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+    ]);
+
+    $rectorConfig->skip([
+        ClosureToArrowFunctionRector::class,
+    ]);
+};
